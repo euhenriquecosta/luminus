5fa2a42478a86ea65e52c39556631936b8ce391b
Henrique <henriquecostapsilva@gmail.com>
2025-06-15 23:47:18 -0300

    feat(use-cases): atualizar caso de uso para criar respostas
    Modifica a classe AnswerQuestionUseCase para retornar um objeto contendo a resposta criada. Atualiza os testes para utilizar um repositório em memória, garantindo a criação e verificação correta das respostas no contexto do fórum.

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,22 +1,26 @@
-import test from 'node:test'
+import { beforeEach, describe, it } from 'node:test'
 import assert from 'node:assert'
 
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question.js'
-import { AnswersRepository } from '@/domain/forum/application/repositories/answers.repository.js'
-import { Answer } from '@/domain/forum/enterprise/entities/answer.js'
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answers.repository.js'
 
-const fakeAnswerRepository: AnswersRepository = {
-  create: async (answer: Answer) => {},
-}
+let inMemoryAnswerRepository: InMemoryAnswerRepository
+let sut: AnswerQuestionUseCase
 
-test('deve ser criado uma resposta', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
-
-  const answer = await answerQuestion.execute({
-    questionId: '1',
-    instructorId: '1',
-    content: 'Nova resposta',
+describe('Answer Question', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswerRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswerRepository)
   })
 
-  assert.equal(answer.content, 'Nova resposta')
+  it('deve ser possivel criar uma resposta', async () => {
+    const { answer } = await sut.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'Nova resposta',
+    })
+    
+    assert.ok(answer.id)
+    assert.equal(inMemoryAnswerRepository.items[0].id, answer.id)
+  })
 })
