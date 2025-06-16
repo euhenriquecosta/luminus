826e4c4ea39e1a42910d9a4b9fd51d7a3b33a720
Henrique <henriquecostapsilva@gmail.com>
2025-06-15 23:47:09 -0300

    feat(use-cases): implementar caso de uso para criar perguntas
    Adiciona a classe CreateQuestionUseCase e seu respectivo teste, permitindo a criação de perguntas no fórum. Um repositório em memória é utilizado para simular a persistência de dados durante os testes.

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,26 @@
+import { beforeEach, describe, it } from 'node:test'
+import assert from 'node:assert'
+
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question.js'
+import { InMemoryQuestionRepository } from 'test/repositories/in-memory-question.repository.js'
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+let sut: CreateQuestionUseCase
+
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository()
+    sut = new CreateQuestionUseCase(inMemoryQuestionRepository)
+  })
+
+  it('deve ser possivel criar uma pergunta', async () => {
+    const { question } = await sut.execute({
+      authorId: '1',
+      title: 'Nova pergunta',
+      content: 'Conteúdo da pergunta',
+    })
+
+    assert.ok(question.id)
+    assert.equal(inMemoryQuestionRepository.items[0].id, question.id)
+  })
+})
